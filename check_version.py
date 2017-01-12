dd6aeaf0bc08a0c1486eb340a83df2b8f1fae0af
saemorris <semorris@ualberta.ca>
2017-01-11 18:04:12 -0700

    changes made to print file to terminal

--- check_version.py
+++ check_version.py
@@ -4,7 +4,8 @@ import requests
 
 print requests.__version__
 
-response = requests.get("http://google.com")
+
+response = requests.get("https://raw.githubusercontent.com/saemorris/cmput404lab1/master/check_version.py")
 
 print response.text
 print response.status_code
